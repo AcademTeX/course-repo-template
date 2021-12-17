62d7bdddf15c676f1527f55028b1fafc3afc2dbc
qwinters <email@quinnwinters.dev>
2021-12-17 22:51:45 +0000

    Deploying to website from @ qwinters/course-repo-template@5bcbdfae2cd73079b8b07cc49c1bdf8f57d98d72 🚀

--- homework/compile-homework.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for assignment in homework* ; do 
-    cd assignment
-    for file in *.tex ; do 
-        latexmk -f -pdf -interaction=nonstopmode $file
-    done 
-    cd .. 
-done
--- notes/compile-notes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for file in *.tex ; do 
-    latexmk -f -pdf -interaction=nonstopmode $file
-done
