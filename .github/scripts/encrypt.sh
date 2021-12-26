e5503adeb539defa19dd4e6222857d4aece3fe27
github-action <contact-me@quinnwinters.dev>
2021-12-26 21:28:36 +0100

    style(ci): Remove commented out code

--- .github/scripts/encrypt.sh
+++ .github/scripts/encrypt.sh
@@ -3,31 +3,6 @@
 CURR_DIR="$(PWD)"
 REPO_DIR="$(git rev-parse --show-toplevel)"
 
-# HOMEWORK_PASSWORD=""
-# while [[ $# -gt 0 ]] ; do 
-#     key="$1"
-#     case $key in 
-#         -p|--password)
-#             HOMEWORK_PASSWORD="$2"
-#             shift
-#             shift
-#             ;;
-#         *)
-#             echo "UNKNOWN ARUGMENT: $key"
-#             exit 1
-#             ;;
-#     esac
-# done 
-
-# # if [[ ! -z "$HOMEWORK_PASSWORD" ]] ; then 
-# #     echo "No password provided, so skipping encryption for homework"
-# #     exit 0
-# # fi
-# if [ -z "$HOMEWORK_PASSWORD" ] ; then 
-#     echo "ERROR: Must provide a homework encryption password"
-#     exit 1
-# fi 
-
 cd $REPO_DIR
 if [[ ! -d encrypted ]] ; then 
     mkdir -p encrypted
